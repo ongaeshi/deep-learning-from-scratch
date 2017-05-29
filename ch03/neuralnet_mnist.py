830c83b1d06098267dede392f5644c061251ab97
ongaeshi <ongaeshi0621@gmail.com>
2017-05-29 23:15:48 +0900

    Fix bug

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -7,6 +7,13 @@ from dataset.mnist import load_mnist
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
 def get_data():
     (x_train, t_train), (x_test, t_test) = \
         load_mnist(normalize=True, flatten=True, one_hot_label=False)
@@ -26,7 +33,7 @@ def predict(network, x):
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
-    y = sigmoid(a3)
+    y = softmax(a3)
 
     return y
 
